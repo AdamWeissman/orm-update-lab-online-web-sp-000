66a1eaf85e40b0f4e32cb868cca05eea62e60f7a
Adam Weissman <adam@comedytragedyepic.com>
2019-11-16 10:34:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,29 @@ class Student
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)   
+  end  
+    
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
   end
+  
+  def self.new_from_db(row)
+  	new_student = self.new(row[1],row[2],row[0])  
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+    
   
 end
