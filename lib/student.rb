0dad1f45a9216095ebb8fe2e4958cd28a14090f4
Adam Weissman <adam@comedytragedyepic.com>
2019-11-16 10:28:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
-    db_connect(sql, self.name, self.grade)
+    self.db_connect(sql, self.name, self.grade)
     self.update
     #end
   end
